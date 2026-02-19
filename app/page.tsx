347c622e9bf49742f77efd2ce563fa8289d778b0
sadeeqzz <sadeeqndanusa5@gmail.com>
2026-02-19 09:59:42 +0100

    hello world

--- app/page.tsx
+++ app/page.tsx
@@ -2,22 +2,37 @@ import Image from "next/image";
 import Link from "next/dist/client/link";
 
 export default function Home() {
-  return (
-    <div className={"min-h-screen bg-gradient-to-bt from-purple-50 to-purple-100 flex items-center justify-center"}>
-      <div className={`container mx-auto px-4 py-16`}>
-        <div className={`text-center`}>
-          <h1 className={`text-5xl font-bold text-gray-900 mb-6`}>
-            Inventory Management
-          </h1>
-          <p className={`text-xl text-gray-900 mb-8 max-w-2xl mx-auto`}>
-              Streamline your inventory tracking with our powerful, easy-to-use management system. track products, monitor stock levels, and gain valuable insights
-          </p>
-          <div className={`flex gap-4 justify-center`}>
-              <Link href={`/sign-in`} className={`bg-purple-600 text-white px-8 py-3 rounded-4xl font-semibold hover:bg-purple-700 transition-colors`} style={{cornerShape: "superellipse(2)"}}>Sign In</Link>
-              <Link href={`#`} className={`bg-white text-purple-600 px-8 py-3 rounded-lg font-semibold border-2 border-purple-600 hover:bg-purple-600 hover:text-white transition-all`}>Learn More</Link>
-          </div>
-        </div>
+   return (
+      <div
+         className={
+            "min-h-screen bg-gradient-to-bt from-purple-50 to-purple-100 flex items-center justify-center"
+         }>
+         <div className={`container mx-auto px-4 py-16`}>
+            <div className={`text-center`}>
+               <h1 className={`text-5xl font-bold text-gray-900 mb-6`}>
+                  Inventory Management
+               </h1>
+               <p className={`text-xl text-gray-900 mb-8 max-w-2xl mx-auto`}>
+                  Streamline your inventory tracking with our powerful,
+                  easy-to-use management system. track products, monitor stock
+                  levels, and gain valuable insights
+               </p>
+               <div className={`flex gap-4 justify-center`}>
+                  <Link
+                     href={`/sign-in`}
+                     className={`bg-purple-600 text-white px-8 py-3 rounded-3xl font-semibold hover:bg-purple-700 transition-colors`}
+                     style={{ cornerShape: "superellipse(2)" }}>
+                     Sign In
+                  </Link>
+                  <Link
+                     href={`#`}
+                     className={`bg-white text-purple-600 px-8 py-3 rounded-3xl font-semibold border-2 border-purple-600 hover:bg-purple-600 hover:text-white transition-all`}
+                     style={{ cornerShape: "superellipse(2)" }}>
+                     Learn More
+                  </Link>
+               </div>
+            </div>
+         </div>
       </div>
-    </div>
-  );
+   );
 }
