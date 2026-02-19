47a3fa29b3992dab3d2fa9544e6122604439dbac
sadeeqzz <sadeeqndanusa5@gmail.com>
2026-02-19 17:42:30 +0100

    next app

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
                <h1 className={`text-5xl font-bold text-gray-900 mb-6`}>
                   Inventory Management
                </h1>
-               <p className={`text-xl text-gray-900 mb-8 max-w-2xl mx-auto`}>
+               <p className={`text-xl text-gray-200 mb-8 max-w-2xl mx-auto`}>
                   Streamline your inventory tracking with our powerful,
                   easy-to-use management system. track products, monitor stock
                   levels, and gain valuable insights
@@ -20,13 +20,13 @@ export default function Home() {
                <div className={`flex gap-4 justify-center`}>
                   <Link
                      href={`/sign-in`}
-                     className={`bg-purple-600 text-white px-8 py-3 rounded-3xl font-semibold hover:bg-purple-700 transition-colors`}
+                     className={`bg-purple-600 text-white px-8 py-3 rounded-2xl font-semibold hover:bg-purple-700 transition-colors`}
                      style={{ cornerShape: "superellipse(2)" }}>
                      Sign In
                   </Link>
                   <Link
                      href={`#`}
-                     className={`bg-white text-purple-600 px-8 py-3 rounded-3xl font-semibold border-2 border-purple-600 hover:bg-purple-600 hover:text-white transition-all`}
+                     className={`bg-white text-purple-600 px-8 py-3 rounded-2xl font-semibold border-2 border-purple-600 hover:bg-purple-600 hover:text-white transition-all`}
                      style={{ cornerShape: "superellipse(2)" }}>
                      Learn More
                   </Link>
